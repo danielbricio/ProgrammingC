e0baa73ba5297a3137be96b7ecd5b023d7cce03d
danielbricio <48757716+danielbricio@users.noreply.github.com>
2022-11-08 10:40:06 -0300

    Atividades

---
+++ Atividade003.c
@@ -0,0 +1,30 @@
+//Atividade 003
+
+
+#include <stdio.h>
+
+	int main()
+	{
+		printf("Escreva uma mensagem na tela: \n");
+		
+		// %d vem do numero inteiro Decimal Integer
+		printf("Fizemos %d programas\n", 3);
+		printf("Fizemos 3 programas");
+		
+		printf("\n");
+		//quebra linhas \n
+		
+		
+		printf("Letra %c apresentada\n", 'X'); //tem que ser colocada entre apostrofe
+		//%c é utilizado para mostrar UM caractere
+		
+		printf("Frase: %s \n", "frase escrita");
+		// s% é utilizado para mostra uma STRING: frase entre apostrofe
+		
+		printf("Um numero qualquer %d \n", 347);	
+		
+		
+		
+		
+		return 0;
+				 } 			
--- exercicio01.c
+++ exercicio01.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+///Exercicio 001
+//DANIEL BRICIO DE CARVALHO SILVA
 
-int main() {
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include "stdlib.h" standard library 
 	
-	printf("Daniel\n");
-	printf("Bricio\n");
-	printf("de\n");
-	printf("C\n");
-	printf("Silva\n");
+	int main() {
+		
+		printf("Daniel\n");
+		printf("Bricio\n");
+		printf("de\n");
+		printf("C\n");
+		printf("Silva\n");
+		
+		
+		
+		//essa função irá pausar o programa após a execução
+		system("pause");
+		return 0;
+	}
 
-	
-	return 0;
-}
+//CASE SENSITIVE: A linguagem C é case sensitive, ou seja; diferencia entre letras maúsculas e minúsculas.
+
+//Cuidado com a identação, pois é uma boa pratica de programação.  facilita o entendimento do codigo. 
