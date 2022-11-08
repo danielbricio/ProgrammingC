1105ef5a8f44246b4d344b5103fdb84289b1eab2
danielbricio <48757716+danielbricio@users.noreply.github.com>
2022-11-08 10:06:14 -0300

    Initial commit

---
+++ exercicio01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	printf("Daniel\n");
+	printf("Bricio\n");
+	printf("de\n");
+	printf("C\n");
+	printf("Silva\n");
+
+	
+	return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	printf("Meu Primeiro Programa!\n");
+	printf("Daniel\n");
+	printf("Bricio\n");
+	
+	//printf é a função que mostrará na tela o conteudo digitado na string
+	//string -> fica entre aspas " "
+	//\n vem de NEW LINE - ele vai fazer uma nova linha
+	
+	
+	return 0;
+	system("pause");
+}
+
+
+
+	//case sensitive linguagens cuja haverá diferença entre maustula e minuscula
