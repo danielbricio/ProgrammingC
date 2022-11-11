22b05e4736e69de7be2e3f8a2859f8f61d8ae527
danielbricio <48757716+danielbricio@users.noreply.github.com>
2022-11-10 22:50:04 -0300

    Variaveis02

--- Variaveis01.c
+++ Variaveis01.c
@@ -3,12 +3,35 @@
 
 
 #include <stdio.h>
+#include <locale.h>
 
 	int main(){
 		
+		setlocale(LC_ALL, "Portuguese");
 		//precisamos defitnir que tipo de dado ela vai armazenar; int 
 		
 		int numero; //declaração de variáveis
+		//tipo lista_de_variaveis
+		//para numeros do tipo INTEIRO
+		
+		//variáveis do mesmo tipo pode ser declaradas em uma unica linha
+		float nota1, nota2 nota3;
+		//numero com casas decimais ou fracionarios utilizanos float
+		
+		char letra;
+		//char para letras ou conjunto de caracteres 'char= character'
+		
+		
+		
+		numero = 37;
+		
+		printf("O valor da variável \"número\" é: %d", numero);
+		
+		numero = 44 + 10;
+		
+		printf("\n\nO novo valor da variável \"número\" é %d ", numero);
+		
+		
 		
 		return 0;
 	}
