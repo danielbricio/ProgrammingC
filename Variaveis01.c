9b7ba2ed9a77de327401b738be9f4eaa7b6299ab
danielbricio <48757716+danielbricio@users.noreply.github.com>
2022-11-10 22:23:35 -0300

    Variaveis01

---
+++ Variaveis01.c
@@ -0,0 +1,14 @@
+//Variaveis são areas de memória do programa utilizada para guardar infomrações.
+//declarar uma variável significa dar um nome a ela(variavel)
+
+
+#include <stdio.h>
+
+	int main(){
+		
+		//precisamos defitnir que tipo de dado ela vai armazenar; int 
+		
+		int numero; //declaração de variáveis
+		
+		return 0;
+	}
