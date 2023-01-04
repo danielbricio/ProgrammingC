59677572eb768c27695681e96d205bacf652fc27
danielbricio <48757716+danielbricio@users.noreply.github.com>
2023-01-04 17:07:22 -0300

    exe035.2

--- exercicio035.c
+++ exercicio035.c
@@ -4,65 +4,13 @@
 
 //DO WHILE - REPITA ATÉ
 
-	int codigo, contPan, contCha, contVas;
-		
-		contPan = contCha = contVas = 0;
-		
-		do{
-			system("cls");
-			
-			printf("\n \n \nMenu de Opções \n");
-			printf("0 - Encerra Programa \n");
-			printf("1 - Incluir Panela \n");
-			printf("2 - Incluir Chaleira \n");
-			printf("3 - Incluir Vassoura \n");
-			printf("4 - Relatório \n");
-			printf("Escolha uma opção \n");
-			scanf("%d", &codigo);
-			
-			switch (codigo){
-				
-				case 0:
-					printf("Encerrando o Programa! \n");
-					break;
-					
-			 	case 1:
-			 		printf("Colando PANELA \n");
-			 		contPan++;
-			 		break;
-	
-			 	case 2:
-			 		printf("Colando CHALEIRA \n");
-			 		contCha++;
-			 		break;
-			 		
-			 	case 3:
-			 		printf("Colando VASSOURA\n");
-			 		contVas++;
-			 		break;
-				
-				case 4:
-					printf("PRODUTOS COLOCADO\n");
-					printf("%d Panelas no estoque \n", contPan);
-					printf("%d Chaleira no estoque \n", contCha);
-					printf("%d Vassoura no estoque \n", contVas);
-			 		break;
-					 			 		
-			 		default:
-			 			printf("Opção Inválida\n");
-			 			break;
-			 			
-			 
-			 }
-				system("pause");
-						
-		}while (codigo != 0);
-		
 
 	int main(){
 		setlocale(LC_ALL, "Portuguese");
 		
-		int codigo;
+		int codigo, totEnt, totSai, disponivel;
+		
+		totEnt = totSai = disponivel = 0;
 		
 		do{
 			system("cls");
@@ -76,8 +24,44 @@
 			printf("Escolha uma opção \n");
 			scanf("%d", &codigo);
 			
+			switch(codigo)
+			{
+				case 0:
+					printf(" Encerrando o Programa \n");
+					break;
+				case 1:
+					printf(" incluindo unidade \n");
+					totEnt++;
+					disponivel++;
+					break;
+				case 2:
+					printf(" excluindo  Unidade \n");
+					totSai++;
+					disponivel--;
+					break;
+				case 3:
+					printf(" Mostrando Disponivel no estoque \n");
+					printf("Disponivel no estoque: %d \n", disponivel);
+					break;
+				case 4:
+					printf(" Mostrando total de entradas no estoque \n");
+					printf("entradas no estoque: %d \n", totEnt);
+					break;
+				case 5:
+					printf("Mostrando total de saidas no estoque \n");
+					break;
+					
+					default: printf("Escolheu uma opção inválida \n");
+					printf("saidas no estoque: %d \n", totSai);
+					break;
+			
 			}while (codigo !=0);
 		
+			
+					
+				
+			}
+			system("pause");
 	
 		
 		return 0;
